d3fc7b36152a294647ec31c521f618240c859cc7
Carl Reyes <reyes.carlj@gmail.com>
2018-05-04 10:50:02 -0400

    Got rid of google maps stuff for now

--- src/components/PlacesMap.js
+++ src/components/PlacesMap.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import MapComponent from './GoogleMap';
 
-const PlacesMap = props => (
+export default() =>
   <div className="col-md-3">
     <h2>Places Map</h2>
-    <MapComponent
-      loadingElement={<h1 className='text-center'>Loading...</h1>}
-      containerElement={<div style={{height: '100%', width: '100%'}} />}
-      mapElement={<div style={{height: '100%', width: '100%'}} />}
-    />
   </div>
-)
-
-export default PlacesMap;
